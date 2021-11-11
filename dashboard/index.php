51b611f8809298b9a16ad9839483810ad16bd971
Orion_XVII <76408216+Clement-XVII@users.noreply.github.com>
2021-11-11 16:50:19 +0100

    Update index.php

--- dashboard/index.php
+++ dashboard/index.php
@@ -32,8 +32,8 @@ if ($data['active'] == 1) {
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="../globe.ico" />
-  <script src="../assets/js/jquery.min.js"></script>
-  <link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
   <title>Home</title>
   <style>
     .bd-placeholder-img {
