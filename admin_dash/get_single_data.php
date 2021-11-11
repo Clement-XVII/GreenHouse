67537814419ee01d4bf493d76cc0340711c0176b
Orion_XVII <76408216+Clement-XVII@users.noreply.github.com>
2021-11-11 16:27:10 +0100

    Delete admin_dash directory

--- admin_dash/get_single_data.php
+++
@@ -1,7 +0,0 @@
-<?php include('../connex.php');
-$id = $_POST['id'];
-$sql = "SELECT * FROM utilisateurs WHERE id='$id' LIMIT 1";
-$query = mysqli_query($connexion,$sql);
-$row = mysqli_fetch_assoc($query);
-echo json_encode($row);
-?>
